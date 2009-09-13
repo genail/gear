56ec4b0fc861673d5a3f3064ff40f988c62b29d3
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2009-09-13 20:16:18 +0200

    * Created server configuration * Repaired error in src/CMakeLists.txt

---
+++ src/Server.cpp
@@ -0,0 +1,17 @@
+/*
+ * Server.cpp
+ *
+ *  Created on: 2009-09-13
+ *      Author: chudy
+ */
+
+#include "Server.h"
+
+Server::Server() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Server::~Server() {
+	// TODO Auto-generated destructor stub
+}

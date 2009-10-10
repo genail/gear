1c737b22906df729ee2f3599f5dc9fab304a0ae8
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2009-10-10 12:54:44 +0200

    * ServerApplication inside the try ... catch clause

--- src/ServerApplication.cpp
+++ src/ServerApplication.cpp
@@ -15,16 +15,20 @@ CL_ClanApplication app(&ServerApplication::main);
 
 int ServerApplication::main(const std::vector<CL_String> &args)
 {
-	CL_SetupCore setup_core;
-	CL_SetupNetwork setup_network;
+	try {
+		CL_SetupCore setup_core;
+		CL_SetupNetwork setup_network;
 
-	CL_ConsoleLogger logger;
+		CL_ConsoleLogger logger;
 
-	Server server(2500);
+		Server server(2500);
 
-	while (true) {
-		CL_KeepAlive::process();
-		server.update(10);
-		CL_System::sleep(10);
+		while (true) {
+			CL_KeepAlive::process();
+			server.update(10);
+			CL_System::sleep(10);
+		}
+	} catch (CL_Exception e) {
+		CL_Console::write_line("Exception thrown: %1", e.message);
 	}
 }

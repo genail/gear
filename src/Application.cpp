81b0f6c8b28aca536a08aefafe1ca4db7ca26524
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2009-11-15 21:27:46 +0100

    Simple cleanup in Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -32,11 +32,13 @@
 #include <ClanLib/display.h>
 #include <ClanLib/application.h>
 
-#ifdef GL1
+#if defined(GL1)
 #include <ClanLib/gl1.h>
-#elif GL2
+#elif defined(GL2)
 #include <ClanLib/gl.h>
-#endif // GL1/2
+#else
+#error must define GL1 or GL2
+#endif // GL1 || GL2
 
 #include "Game.h"
 #include "graphics/GameWindow.h"
@@ -65,131 +67,117 @@ CL_ClanApplication app(&Application::main);
 
 int Application::main(const std::vector<CL_String> &args)
 {
-//	if (args.size() < 3) {
-//		CL_Console::write_line("usage: ./game server_addr nickname");
-//		return 1;
-//	}
-//
-//	const CL_String serverAddrPort = args[1];
-//	const CL_String nickname = args[2];
-
-	std::string stdinNickname, stdinServer, stdinConnect;
-
-//	std::cout << "Player name: ";
-//	std::cin >> stdinNickname;
-//
-//	do {
-//		std::cout << "Do you want to connect to server? (y/n): ";
-//		std::cin >> stdinConnect;
-//	} while (stdinConnect != "y" && stdinConnect != "n");
-//
-//	if (stdinConnect == "y") {
-//		std::cout << "Please specify the server to connect to. The acceptable format is HOST, HOST:PORT or write anything to ." << std::endl;
-//		std::cout << "Server addr: ";
-//		std::cin >> stdinServer;
-//	}
-//
-//	const CL_String serverAddrPort = stdinServer;
-//	const CL_String nickname = stdinNickname;
-	const CL_String serverAddrPort = "localhost";
-	const CL_String nickname = "n";
-
-	// set default properties
-#ifndef NDEBUG
-	Properties::setProperty("debug.draw_checkpoints", false);
-	Properties::setProperty("debug.draw_vectors", false);
-#endif //NDEBUG
-
-	// read args properties
-	for (std::vector<CL_String>::const_iterator itor = args.begin(); itor != args.end(); ++itor) {
-		if (itor->substr(0, 2) == "-P") {
-			const std::vector<CL_TempString> parts = CL_StringHelp::split_text(itor->substr(2), "=");
-
-			if (parts.size() != 2) {
-				CL_Console::write_line(CL_String8("cannot parse ") + *itor);
-				continue;
-			}
-
-			Properties::setProperty(parts[0], parts[1]);
-		}
-	}
-
-	Gfx::Stage::m_width = 800;
-	Gfx::Stage::m_height = 600;
-
-	// Setup clanlib modules:
-	CL_SetupCore 	setup_core;
-	CL_SetupDisplay setup_display;
-	CL_SetupGUI 	setup_gui;
-
-#ifdef GL1
-	CL_SetupGL1     setup_gl;
-#elif GL2
-	CL_SetupGL     setup_gl;
-#endif // GL1/2
-	CL_SetupNetwork setup_network;
-
-	CL_ConsoleLogger logger;
-
-	CL_OpenGLWindowDescription desc;
-	desc.set_title("Gear");
-	desc.set_size(CL_Size(Gfx::Stage::m_width, Gfx::Stage::m_height), false);
-	desc.set_multisampling(4);
-
-	CL_DisplayWindow display_window(desc);
-
-	// load resources
-	CL_ResourceManager resources("resources/resources.xml");
-	Gfx::Stage::m_resourceManager = &resources;
-
-	CL_GUIWindowManagerTexture wm(display_window);
-
-	// load GUI
-	CL_GUIManager gui;
-	CL_GUIThemeDefault theme;
-//	CL_GUIWindowManagerSystem window_manager;
-
-	gui.set_window_manager(wm);
-
-#ifdef GL1
-	//  Note - If you are using the GL1 target, you will get a perfomance increase by enabling these 2 lines
-	//   It reduces the number of internal CL_FrameBuffer swaps. The GL1 target (OpenGL 1.3), performs this slowly
-	//   Setting the texture group here, lets the GUI Texture Window Manager know the optimum texture size of all root components
-	CL_TextureGroup texture_group(display_window.get_gc(), CL_Size(512, 512));
-	wm.set_texture_group(texture_group);
-#endif //GL1
-
-	CL_ResourceManager res("resources/GUIThemeAeroPacked/resources.xml");
-	CL_CSSDocument css_document;
-	css_document.load("resources/GUIThemeAeroPacked/theme.css");
-
-	gui.set_css_document(css_document);
-	theme.set_resources(res);
-	gui.set_theme(theme);
-
-	CL_DisplayWindowDescription guiDesc("Gear");
-	guiDesc.set_position(CL_Rect(0, 0, Gfx::Stage::getWidth(), Gfx::Stage::getHeight()), true);
-
-	GameWindow gameWindow(&gui, guiDesc);
-
-	// load debug layer
-	DebugLayer debugLayer;
-	Gfx::Stage::m_debugLayer = &debugLayer;
-
-	// build race game
 
 	try {
+
+		// set default properties
+#ifndef NDEBUG
+		Properties::setProperty("debug.draw_checkpoints", false);
+		Properties::setProperty("debug.draw_vectors", false);
+#endif //NDEBUG
+
+		// read args properties
+		for (std::vector<CL_String>::const_iterator itor = args.begin(); itor != args.end(); ++itor) {
+			if (itor->substr(0, 2) == "-P") {
+				const std::vector<CL_TempString> parts = CL_StringHelp::split_text(itor->substr(2), "=");
+
+				if (parts.size() != 2) {
+					CL_Console::write_line(CL_String8("cannot parse ") + *itor);
+					continue;
+				}
+
+				Properties::setProperty(parts[0], parts[1]);
+			}
+		}
+
+		// modules setup
+		CL_Console::write_line("initializing");
+
+		CL_SetupCore 	setup_core;
+		CL_ConsoleLogger logger;
+
+		cl_log_event("init", "initializing display");
+		CL_SetupDisplay setup_display;
+
+		Gfx::Stage::m_width = 800;
+		Gfx::Stage::m_height = 600;
+
+		cl_log_event("init", "display will be %1 x %2", Gfx::Stage::m_width, Gfx::Stage::m_height);
+
+		cl_log_event("init", "initializing gui");
+		CL_SetupGUI 	setup_gui;
+
+		cl_log_event("init", "initializing network");
+		CL_SetupNetwork setup_network;
+
+#if defined(GL1)
+		cl_log_event("init", "initializing OpenGL 1.x");
+		CL_SetupGL1     setup_gl;
+
+		CL_DisplayWindow displayWindow("Gear", Gfx::Stage::m_width, Gfx::Stage::m_height);
+#else defined(GL2)
+		cl_log_event("init", "initializing OpenGL 2.x");
+		CL_SetupGL     setup_gl;
+
+		CL_OpenGLWindowDescription displayWindowDescription;
+
+		displayWindowDescription.set_title("Gear");
+		displayWindowDescription.set_size(CL_Size(Gfx::Stage::m_width, Gfx::Stage::m_height), false);
+		displayWindowDescription.set_multisampling(4);
+
+		CL_DisplayWindow displayWindow(displayWindowDescription);
+#endif // GL1 || GL2
+
+		cl_log_event("init", "loading gui components");
+
+		CL_GUIWindowManagerTexture windowManager(displayWindow);
+
+		CL_GUIManager guiManager;
+		CL_GUIThemeDefault guiTheme;
+
+		guiManager.set_window_manager(windowManager);
+
+#ifdef GL1
+		//  Note - If you are using the GL1 target, you will get a perfomance increase by enabling these 2 lines
+		//   It reduces the number of internal CL_FrameBuffer swaps. The GL1 target (OpenGL 1.3), performs this slowly
+		//   Setting the texture group here, lets the GUI Texture Window Manager know the optimum texture size of all root components
+		CL_TextureGroup texture_group(displayWindow.get_gc(), CL_Size(512, 512));
+		windowManager.set_texture_group(texture_group);
+#endif // GL1
+
+		CL_ResourceManager guiResources("resources/GUIThemeAeroPacked/resources.xml");
+		CL_CSSDocument cssDocument;
+		cssDocument.load("resources/GUIThemeAeroPacked/theme.css");
+
+		guiManager.set_css_document(cssDocument);
+		guiTheme.set_resources(guiResources);
+		guiManager.set_theme(guiTheme);
+
+		// load resources
+		cl_log_event("init", "loading general resources");
+
+		CL_ResourceManager generalResources("resources/resources.xml");
+		Gfx::Stage::m_resourceManager = &generalResources;
+
+		CL_DisplayWindowDescription guiDesc("Gear");
+		guiDesc.set_position(CL_Rect(0, 0, Gfx::Stage::getWidth(), Gfx::Stage::getHeight()), true);
+
+		GameWindow gameWindow(&guiManager, guiDesc);
+
+		// load debug layer
+		DebugLayer debugLayer;
+		Gfx::Stage::m_debugLayer = &debugLayer;
+
+		cl_log_event("init", "setting scene container");
 
 		SceneContainer sceneContainer(&gameWindow);
 		Game::getInstance().setSceneContainer(&sceneContainer);
 
-		Gfx::Stage::pushScene(&sceneContainer.getMainMenuScene());
 
-		// run the gui
-		gui.exec(true);
-//		while (!gui.exec(false)) {
-//
-//		}
+		cl_log_event("init", "launching main scene");
+
+		Gfx::Stage::pushScene(&sceneContainer.getMainMenuScene());
+		guiManager.exec(true);
 
 	} catch (CL_Exception e) {
 		CL_Console::write_line(e.message);

d6314bab67399e668d2664c866823085f6d20c20
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2010-01-05 22:36:14 +0100

    Added TrackSegment and TrackTriangulator classes

---
+++ src/logic/race/level/TrackTriangulator.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2009, Piotr Korzuszek
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "TrackTriangulator.h"
+
+#include <map>
+
+#include "logic/race/level/TrackSegment.h"
+
+namespace Race {
+
+class TrackTriangulatorImpl
+{
+	public:
+
+		typedef std::map<int, TrackSegment> TIntSegmentMap;
+
+
+		TIntSegmentMap m_intSegMap;
+};
+
+TrackTriangulator::TrackTriangulator()
+{
+}
+
+TrackTriangulator::~TrackTriangulator()
+{
+}
+
+void TrackTriangulator::triangulate(const Track &p_track, int p_segment)
+{
+	if (p_segment == -1) {
+		const int pointCount = p_track.getPointCount();
+
+		for (int i = 0; i < pointCount; ++i) {
+			triangulate(p_track, i);
+		}
+	}
+
+
+}
+
+}

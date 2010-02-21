206a4edb66279671ad1c61179087db087a827a36
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2010-02-21 09:44:21 +0100

    TyreStripes optimalized
    But there's a bug that removes some tracks.

--- src/gfx/race/level/TyreStripes.cpp
+++ src/gfx/race/level/TyreStripes.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, Piotr Korzuszek
+ * Copyright (c) 2009-2010, Piotr Korzuszek
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,6 +38,13 @@
 
 namespace Gfx {
 
+const int IMMUTABLE_STRIPE_LIMIT = 32;
+
+const CL_Colorf STRIPE_COLOR(0.0f, 0.0f, 0.0f, 0.15f);
+const CL_Vec4f STRIPE_COLOR_VEC(
+		STRIPE_COLOR.r, STRIPE_COLOR.g, STRIPE_COLOR.b, STRIPE_COLOR.a
+);
+
 class Stripe {
 
 	public:
@@ -48,8 +55,11 @@ class Stripe {
 		/** Owner to continue last stripe */
 		const Race::Car *m_owner;
 
+		/** If this stripe is still mutable?  */
+		bool m_mutable;
+
 		Stripe(const CL_Pointf &p_from, const CL_Pointf &p_to, const Race::Car *p_owner) :
-			m_from(p_from), m_to(p_to), m_owner(p_owner) {}
+			m_from(p_from), m_to(p_to), m_owner(p_owner), m_mutable(true) {}
 
 
 		float length() const { return m_from.distance(m_to); }
@@ -60,28 +70,110 @@ class Stripe {
 
 };
 
+// array of primitives for quicker drawing
+class StripeArr
+{
+	private:
+
+		/** Verticle array */
+		CL_Vec2f *const m_verticles;
+
+		/** Color array */
+		CL_Vec4f *const m_colors;
+
+		/** Compiled state */
+		bool m_compiled;
+
+		/** Primitives array */
+		CL_PrimitivesArray *m_priArr;
+
+		void compile(CL_GraphicContext &p_gc) {
+			G_ASSERT(!m_compiled);
+
+			m_priArr = new CL_PrimitivesArray(p_gc);
+			m_priArr->set_attributes(0, m_verticles);
+			m_priArr->set_attributes(1, m_colors);
+
+			m_compiled = true;
+		}
+
+	public:
+
+		/** Array size */
+		const int m_size;
+
+		StripeArr(int p_size) :
+			m_verticles(new CL_Vec2f[p_size * 2]),
+			m_colors(new CL_Vec4f[p_size * 2]),
+			m_size(p_size),
+			m_compiled(false),
+			m_priArr(NULL)
+		{ /* empty */ }
+
+		~StripeArr() {
+			if (m_priArr) {
+				delete m_priArr;
+			}
+			delete[] m_colors;
+			delete[] m_verticles;
+		}
+
+		void add(int p_idx, const Stripe &p_stripe) {
+			G_ASSERT(!m_compiled);
+			G_ASSERT(p_idx >= 0 && p_idx < m_size);
+
+			const int idx2 = p_idx * 2;
+
+			m_verticles[idx2] = p_stripe.m_from;
+			m_verticles[idx2 + 1] = p_stripe.m_to;
+
+			m_colors[idx2] = STRIPE_COLOR_VEC;
+			m_colors[idx2 + 1] = STRIPE_COLOR_VEC;
+		}
+
+		void draw(CL_GraphicContext &p_gc) {
+			if (!m_compiled) {
+				compile(p_gc);
+			}
+
+			p_gc.set_program_object(cl_program_color_only);
+			p_gc.draw_primitives(cl_lines, m_size, *m_priArr);
+		}
+};
+
 
 class TyreStripesImpl
 {
 	public:
 
 		typedef std::list<Stripe> TStripeList;
+		typedef std::deque<StripeArr*> TStripeArrList;
 		typedef std::map<const Race::Car*, CL_Pointf> TDriftPointMap;
 
 		/** Level at what stripes are drawn */
 		Race::Level m_level;
 
-		/** Stripes container */
+		/** Stripes container. First stripe is the youngest one. */
 		TStripeList m_stripes;
+
+		/** Stripe arrays containter */
+		TStripeArrList m_stripeArrays;
+
+		/** Immutable stripes count (for optimalization) */
+		int m_immutableStripeCnt;
 
 		/** Last drift point map */
 		TDriftPointMap m_lastDriftMap;
 
 
 		TyreStripesImpl(const Race::Level &p_level) :
-			m_level(p_level)
+			m_level(p_level),
+			m_immutableStripeCnt(0)
 		{ /* empty */ }
 
+		~TyreStripesImpl() {
+			clear();
+		}
 
 		void add(
 				const CL_Pointf &p_from,
@@ -91,10 +183,18 @@ class TyreStripesImpl
 
 		void add4WheelStripe(const Race::Car &p_car, const CL_Pointf &p_from);
 
+
+		void clear();
+
+
 		bool equals(
 				const CL_Pointf &p_a, const CL_Pointf &p_b,
 				float p_precission
 		);
+
+
+		/** Splits immutable sprites from mutable */
+		void splitImmutable();
 
 };
 
@@ -115,24 +215,26 @@ void TyreStripesImpl::add(
 		const Race::Car *p_owner
 )
 {
-	static const unsigned STRIPE_LIMIT = 200;
 	static const unsigned STRIPE_LENGTH_LIMIT = 30;
 	// how many last stripes that belongs to this car should I check?
-	static const unsigned BACK_SEARCH_LIMIT = 4;
+	static const int BACK_SEARCH_LIMIT = 4;
 	// point equals check precission
 	static const float EQUAL_CHECK_PRECISSION = 3.0f;
 
 	// search for four last stripes of this car and check if I can merge
 	// this stripe to the last one
 	bool merged = false;
-	unsigned foundCount = 0;
+	int foundCount = 0;
+
+	// last found stripe
+	Stripe *lastFound = NULL;
 
 	for (
 			TStripeList::iterator itor = m_stripes.begin();
 			itor != m_stripes.end();
 			++itor
 	) {
-		Stripe s = *itor;
+		Stripe &s = *itor;
 
 		if (s.m_owner == p_owner) {
 
@@ -143,9 +245,10 @@ void TyreStripesImpl::add(
 					equals(s.m_to, p_from, EQUAL_CHECK_PRECISSION)
 					&& s.length() < STRIPE_LENGTH_LIMIT
 				) {
-				Stripe copy = s;
+				G_ASSERT(s.m_mutable);
 
 				// remove old stripe
+				Stripe copy = s;
 				m_stripes.erase(itor);
 
 				// and construct new one
@@ -156,6 +259,7 @@ void TyreStripesImpl::add(
 			}
 
 			if (foundCount == BACK_SEARCH_LIMIT) {
+				lastFound = &s;
 				break;
 			}
 		}
@@ -164,11 +268,13 @@ void TyreStripesImpl::add(
 	if (!merged) {
 		// when not merged, then create a new stripe
 		m_stripes.push_front(Stripe(p_from, p_to, p_owner));
-	}
 
-	// remove all stripes above the limit
-	if (m_stripes.size() > STRIPE_LIMIT) {
-		m_stripes.pop_back(); // there will be always one stripe to much
+		// set the last found as immutable
+		if (lastFound) {
+			lastFound->m_mutable = false;
+			m_immutableStripeCnt++;
+		}
+
 	}
 
 }
@@ -184,8 +290,20 @@ bool TyreStripesImpl::equals(
 
 void TyreStripes::clear()
 {
-	m_impl->m_stripes.clear();
-	m_impl->m_lastDriftMap.clear();
+	m_impl->clear();
+}
+
+void TyreStripesImpl::clear()
+{
+	m_stripes.clear();
+	m_lastDriftMap.clear();
+
+
+	foreach (StripeArr *sarr, m_stripeArrays) {
+		delete sarr;
+	}
+
+	m_stripeArrays.clear();
 }
 
 void TyreStripes::update()
@@ -214,6 +332,11 @@ void TyreStripes::update()
 		}
 	}
 
+	// check immutable stripes count for optimalization
+	if (m_impl->m_immutableStripeCnt >= IMMUTABLE_STRIPE_LIMIT) {
+		cl_log_event(LOG_DEBUG, "split stripes: %1, %2", m_impl->m_stripes.size(), m_impl->m_stripeArrays.size());
+		m_impl->splitImmutable();
+	}
 }
 
 void TyreStripesImpl::add4WheelStripe(
@@ -254,6 +377,45 @@ void TyreStripesImpl::add4WheelStripe(
 	}
 }
 
+void TyreStripesImpl::splitImmutable()
+{
+	static const unsigned ARRAYS_LIMIT = 8;
+
+	// I need to skip first immutable stripes in order to take back
+	// the oldest ones
+	int skipCount = m_immutableStripeCnt - IMMUTABLE_STRIPE_LIMIT;
+	m_immutableStripeCnt -= IMMUTABLE_STRIPE_LIMIT;
+
+	StripeArr *arr = new StripeArr(IMMUTABLE_STRIPE_LIMIT);
+	int idx = 0;
+
+	for (
+			TStripeList::iterator itor = m_stripes.begin();
+			itor != m_stripes.end();
+	) {
+		Stripe &s = *itor;
+		if (!s.m_mutable) {
+
+			// skip first stripes if necessary
+			if (skipCount == 0) {
+				arr->add(idx++, s);
+				m_stripes.erase(itor++);
+			} else {
+				skipCount--;
+				itor++;
+			}
+		} else {
+			itor++;
+		}
+	}
+
+	m_stripeArrays.push_back(arr);
+
+	if (m_stripeArrays.size() > ARRAYS_LIMIT) {
+		m_stripeArrays.pop_front();
+	}
+}
+
 void TyreStripes::draw(CL_GraphicContext &p_gc)
 {
 	CL_Pen oldPen = p_gc.get_pen();
@@ -262,13 +424,16 @@ void TyreStripes::draw(CL_GraphicContext &p_gc)
 	newPen.set_line_width(3);
 	p_gc.set_pen(newPen);
 
-	static const CL_Colorf color(0.0f, 0.0f, 0.0f, 0.5f);
+	// draw arrays
+	foreach (StripeArr *sarr, m_impl->m_stripeArrays) {
+		sarr->draw(p_gc);
+	}
 
 	foreach (const Stripe &stripe, m_impl->m_stripes) {
 		const CL_Pointf &from = stripe.getFromPoint();
 		const CL_Pointf &to = stripe.getToPoint();
 
-		CL_Draw::line(p_gc, from, to, color);
+		CL_Draw::line(p_gc, from, to, STRIPE_COLOR);
 	}
 }
 

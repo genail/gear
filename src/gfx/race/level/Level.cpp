b277f4e9f25842e572fd8179ebb892b5919c7fb4
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2010-03-06 12:42:20 +0100

    New street texture, sand and cracks drawing code
    Still without sand and cracks textures.

--- src/gfx/race/level/Level.cpp
+++ src/gfx/race/level/Level.cpp
@@ -47,9 +47,32 @@ namespace Gfx
 const CL_Vec4f WHITE(1.0f, 1.0f, 1.0f, 1.0f);
 const int HELPER_SIZE = 4;
 
+const int CRACKS_COUNT = 5;
+
+// length of street in meters
+const float STREET_TILE_LENGTH_M = 30.0f;
+
 class LevelImpl
 {
 	public:
+
+		enum Direction {
+			D_LEFT,
+			D_RIGHT
+		};
+
+		struct Crack {
+			CL_Pointf m_position;
+
+			/** m_cracks index */
+			int m_crackId;
+
+			Crack(const CL_Pointf &p_pos, int p_id) :
+				m_position(p_pos),
+				m_crackId(p_id)
+			{ /* empty */ }
+		};
+
 
 		const Race::Level m_levelLogic;
 
@@ -58,12 +81,28 @@ class LevelImpl
 		const Race::TrackTriangulator &m_triangulator;
 
 
-		/** street texture */
+		/** Street texture */
 		CL_Texture m_streetTexture;
 
+		/** Sand texture */
+		CL_Texture m_sandTexture;
 
-		/** trackpoints distances from start [seg_id][point_id] */
+
+		// crack sprites in street
+		CL_Sprite m_crackSprites[CRACKS_COUNT];
+
+		// cracks collection to draw
+		std::vector<Crack> m_crackPositions;
+
+
+		/** center track point distances from start [seg_id][point_id] */
 		std::vector<std::vector<float> > m_distances;
+
+		/** Left track side distances */
+		std::vector<std::vector<float> > m_ldistances;
+
+		/** Right track side distances */
+		std::vector<std::vector<float> > m_rdistances;
 
 		/** Distance from last point to start point */
 		float m_distanceToStart;
@@ -95,10 +134,23 @@ class LevelImpl
 		}
 
 
-		void drawQuads(CL_GraphicContext &p_gc);
+		void drawTrack(CL_GraphicContext &p_gc);
+
+		void drawQuads(
+				CL_GraphicContext &p_gc,
+				int p_segmentId
+		);
+
+		void drawSand(
+				CL_GraphicContext &p_gc,
+				int p_segmentId,
+				const std::vector<Race::TrackSegment::PointPair> &p_pairs,
+				Direction p_direction
+		);
 
 		void drawQuad(
 				CL_GraphicContext &p_gc,
+				const CL_Texture &p_texture,
 				const CL_Pointf &p_a,
 				const CL_Pointf &p_b,
 				const CL_Pointf &p_c,
@@ -109,6 +161,9 @@ class LevelImpl
 				const CL_Vec2f &p_tcd
 		);
 
+		/** Draw street cracks */
+		void drawCracks(CL_GraphicContext &p_gc);
+
 		void drawStartLine(CL_GraphicContext &p_gc);
 
 		void drawObjects(CL_GraphicContext &p_gc);
@@ -117,6 +172,13 @@ class LevelImpl
 		// loaders
 
 		void loadTrackTexture(CL_GraphicContext &p_gc);
+
+		void loadSandTexture(CL_GraphicContext &p_gc);
+
+		void loadCracks(CL_GraphicContext &p_gc);
+
+
+		void expand(CL_Rectf *p_rect, const CL_Pointf &p_point);
 
 };
 
@@ -139,113 +201,202 @@ Level::~Level()
 
 void Level::draw(CL_GraphicContext &p_gc)
 {
-	m_impl->drawQuads(p_gc);
+	m_impl->drawTrack(p_gc);
 //	m_impl->drawTriangles(p_gc);
+	m_impl->drawCracks(p_gc);
 	m_impl->drawStartLine(p_gc);
 	m_impl->drawObjects(p_gc);
 }
 
-void LevelImpl::drawQuads(CL_GraphicContext &p_gc)
+void LevelImpl::drawTrack(CL_GraphicContext &p_gc)
 {
-	// length of street in meters
-	const float STREET_TILE_LENGTH_M = 30.0f;
-
 	const Race::Track &track = m_levelLogic.getTrack();
 	const int trackPointCount = track.getPointCount();
 
 	const CL_Rectf &viewportBounds = m_viewport.getWorldClipRect();
 
 	for (int i = 0; i < trackPointCount; ++i) {
+
+		const int nextIdx = Math::Integer::clamp(i + 1, 0, trackPointCount - 1);
+
 		const Race::TrackSegment &seg = m_triangulator.getSegment(i);
 
+		CL_Rectf bounds = seg.getBounds();
+		expand(&bounds, m_triangulator.getFirstLeftPoint(nextIdx));
+		expand(&bounds, m_triangulator.getFirstRightPoint(nextIdx));
+
 		// run only if this segment is visible
-		if (viewportBounds.is_overlapped(seg.getBounds())) {
+		if (viewportBounds.is_overlapped(bounds)) {
+			drawSand(p_gc, i, seg.getPointPairs(), D_LEFT);
+			drawSand(p_gc, i, seg.getPointPairs(), D_RIGHT);
+			drawQuads(p_gc, i);
+		}
 
-			const std::vector<Race::TrackSegment::PointPair> &pairs =
-					seg.getPointPairs();
+	}
+}
 
-			const int pairsCount = static_cast<signed>(pairs.size());
-			CL_Pointf prevLeft, prevRight;
+void LevelImpl::drawQuads(
+		CL_GraphicContext &p_gc,
+		int p_segmentId
+)
+{
+	const std::vector<Race::TrackSegment::PointPair> &p_pairs =
+			m_triangulator.getSegment(p_segmentId).getPointPairs();
 
-			for (int pairIdx = 0; pairIdx < pairsCount; ++pairIdx) {
+	// next segment index
+	const Race::Track &track = m_levelLogic.getTrack();
+	const int trackPointCount = track.getPointCount();
+	const int nextSegId = Math::Integer::clamp(
+			p_segmentId + 1, 0, trackPointCount - 1
+	);
 
-				const CL_Pointf &currLeft = pairs[pairIdx].m_left;
-				const CL_Pointf &currRight = pairs[pairIdx].m_right;
+	const int pairsCount = static_cast<signed>(p_pairs.size());
+	CL_Pointf prevLeft, prevRight;
+	CL_Pointf currLeft, currRight;
+	float bDist, fDist;
 
-				if (pairIdx != 0) {
-					const float bDist = m_distances[i][pairIdx - 1];
-					const float fDist = m_distances[i][pairIdx];
+	// draw each quad
+	for (int pairIdx = 0; pairIdx <= pairsCount; ++pairIdx) {
 
-					const float bCoord =
-							fmod(bDist, STREET_TILE_LENGTH_M)
-							/ STREET_TILE_LENGTH_M;
+		if (pairIdx < pairsCount) {
+			currLeft = p_pairs[pairIdx].m_left;
+			currRight = p_pairs[pairIdx].m_right;
+		} else {
+			// last quad is connecting this segment with next one
+			currLeft = m_triangulator.getFirstLeftPoint(nextSegId);
+		    currRight = m_triangulator.getFirstRightPoint(nextSegId);
+		}
 
-					const float fCoord =
-							bCoord + (fDist - bDist)/ STREET_TILE_LENGTH_M;
+		if (pairIdx != 0) {
 
-
-					drawQuad(
-							p_gc,
-							prevLeft,
-							prevRight,
-							currRight,
-							currLeft,
-							CL_Vec2f(0.0f, bCoord),
-							CL_Vec2f(1.0f, bCoord),
-							CL_Vec2f(1.0f, fCoord),
-							CL_Vec2f(0.0f, fCoord)
-					);
-				}
-
-				prevLeft = currLeft;
-				prevRight = currRight;
+			if (pairIdx < pairsCount) {
+				bDist = m_distances[p_segmentId][pairIdx - 1];
+				fDist = m_distances[p_segmentId][pairIdx];
+			} else {
+				// connector
+				bDist = m_distances[p_segmentId][pairIdx - 1];
+				fDist = m_distances[nextSegId][0];
 			}
 
+			const float bCoord =
+					fmod(bDist, STREET_TILE_LENGTH_M)
+					/ STREET_TILE_LENGTH_M;
+
+			const float fCoord =
+					bCoord + (fDist - bDist)/ STREET_TILE_LENGTH_M;
+
+
+			drawQuad(
+					p_gc,
+					m_streetTexture,
+					prevLeft,
+					prevRight,
+					currRight,
+					currLeft,
+					CL_Vec2f(0.0f, bCoord),
+					CL_Vec2f(1.0f, bCoord),
+					CL_Vec2f(1.0f, fCoord),
+					CL_Vec2f(0.0f, fCoord)
+			);
 		}
 
-		// fill the gaps between two segments
-		// FIXME: check when to not draw this
-		const CL_Pointf &prevLeft = m_triangulator.getLastLeftPoint(i);
-		const CL_Pointf &prevRight = m_triangulator.getLastRightPoint(i);
+		prevLeft = currLeft;
+		prevRight = currRight;
+	}
+}
 
-		const int nextIdx = i + 1 < trackPointCount ? i + 1 : 0;
-		const CL_Pointf &nextLeft = m_triangulator.getFirstLeftPoint(nextIdx);
-		const CL_Pointf &nextRight = m_triangulator.getFirstRightPoint(nextIdx);
+void LevelImpl::drawSand(
+		CL_GraphicContext &p_gc,
+		int p_segmentId,
+		const std::vector<Race::TrackSegment::PointPair> &p_pairs,
+		Direction p_direction
+)
+{
+	const std::vector<std::vector<float> > &distances =
+			p_direction == D_LEFT ? m_ldistances : m_rdistances;
 
-		const float bDist = m_distances[i][m_distances[i].size() - 2];
-		float fDist = m_distances[nextIdx][0];
+	const int pairsCount = static_cast<signed>(p_pairs.size());
 
-		if (fDist < bDist) {
-			// this is end point to start point distance
-			fDist = m_distanceToStart;
+	// far coordinates (away from track)
+	CL_Pointf nearPrev, nearCurr, farA, farB;
+
+	// previous point on wanted side
+	CL_Pointf prev;
+	// previous point on oposite side
+	CL_Pointf prevOposite;
+
+	for (int pairIdx = 0; pairIdx < pairsCount; ++pairIdx) {
+
+		const Race::TrackSegment::PointPair &pair = p_pairs[pairIdx];
+
+		const CL_Pointf &curr =
+				p_direction == D_LEFT ? pair.m_left : pair.m_right;
+		const CL_Pointf &currOposite =
+				p_direction == D_LEFT ? pair.m_right : pair.m_left;
+
+		if (pairIdx != 0) {
+			const float bDist = distances[p_segmentId][pairIdx - 1];
+			const float fDist = distances[p_segmentId][pairIdx];
+
+			const float bCoord =
+					fmod(bDist, STREET_TILE_LENGTH_M)
+					/ STREET_TILE_LENGTH_M;
+
+			const float fCoord =
+					bCoord + (fDist - bDist)/ STREET_TILE_LENGTH_M;
+
+			// calculate far coords
+			farA = prev + ((prev - prevOposite) * 0.25);
+			farB = curr + ((curr - currOposite) * 0.25);
+
+			nearPrev = prev;
+			nearCurr = curr;
+
+			nearPrev += (prevOposite - prev) * 0.04;
+			nearCurr += (currOposite - curr) * 0.04;
+
+			switch (p_direction) {
+				case D_LEFT:
+					drawQuad(
+							p_gc,
+							m_sandTexture,
+							nearPrev, nearCurr,
+							farB, farA,
+							CL_Vec2f(0.0f, bCoord),
+							CL_Vec2f(0.0f, fCoord),
+							CL_Vec2f(1.0f, fCoord),
+							CL_Vec2f(1.0f, bCoord)
+					);
+
+					break;
+
+				case D_RIGHT:
+					drawQuad(
+							p_gc,
+							m_sandTexture,
+							nearCurr, nearPrev,
+							farA, farB,
+							CL_Vec2f(0.0f, fCoord),
+							CL_Vec2f(0.0f, bCoord),
+							CL_Vec2f(1.0f, bCoord),
+							CL_Vec2f(1.0f, fCoord)
+					);
+
+					break;
+
+				default:
+					G_ASSERT(0);
+			}
 		}
 
-
-		// FIXME: this is a copy of above code!
-		const float bCoord =
-						fmod(bDist, STREET_TILE_LENGTH_M)
-						/ STREET_TILE_LENGTH_M;
-
-		const float fCoord =
-				bCoord + (fDist - bDist)/ STREET_TILE_LENGTH_M;
-
-		drawQuad(
-				p_gc,
-				prevLeft,
-				prevRight,
-				nextRight,
-				nextLeft,
-				CL_Vec2f(0.0f, bCoord),
-				CL_Vec2f(1.0f, bCoord),
-				CL_Vec2f(1.0f, fCoord),
-				CL_Vec2f(0.0f, fCoord)
-		);
-
+		prev = curr;
+		prevOposite = currOposite;
 	}
 }
 
 void LevelImpl::drawQuad(
 		CL_GraphicContext &p_gc,
+		const CL_Texture &p_texture,
 		const CL_Pointf &p_a,
 		const CL_Pointf &p_b,
 		const CL_Pointf &p_c,
@@ -269,7 +420,7 @@ void LevelImpl::drawQuad(
 	m_helperTexCoords[3] = p_tcd;
 
 	p_gc.set_program_object(cl_program_single_texture);
-	p_gc.set_texture(0, m_streetTexture);
+	p_gc.set_texture(0, p_texture);
 
 	// draw
 	p_gc.draw_primitives(cl_quads, 4, *m_helperArr);
@@ -283,6 +434,19 @@ void LevelImpl::drawQuad(
 	CL_Draw::line(p_gc, p_c, p_d, CL_Colorf::purple);
 	CL_Draw::line(p_gc, p_d, p_a, CL_Colorf::purple);
 #endif
+}
+
+void LevelImpl::drawCracks(CL_GraphicContext &p_gc)
+{
+	float x, y;
+
+	foreach (const Crack &crack, m_crackPositions) {
+
+		x = crack.m_position.x;
+		y = crack.m_position.y;
+
+		m_crackSprites[crack.m_crackId].draw(p_gc, x, y);
+	}
 }
 
 void LevelImpl::drawStartLine(CL_GraphicContext &p_gc)
@@ -342,7 +506,10 @@ void LevelImpl::drawObjects(CL_GraphicContext &p_gc)
 void Level::load(CL_GraphicContext &p_gc)
 {
 	Drawable::load(p_gc);
+
 	m_impl->loadTrackTexture(p_gc);
+	m_impl->loadSandTexture(p_gc);
+	m_impl->loadCracks(p_gc);
 
 	// prepare helper array
 	m_impl->m_helperArr = new CL_PrimitivesArray(p_gc);
@@ -375,6 +542,8 @@ void LevelImpl::loadTrackTexture(CL_GraphicContext &p_gc)
 	// m_distances[segment_id][point_id]
 
 	m_distances.reserve(count);
+	m_ldistances.reserve(count);
+	m_rdistances.reserve(count);
 
 	G_ASSERT(count >= 1);
 
@@ -384,6 +553,8 @@ void LevelImpl::loadTrackTexture(CL_GraphicContext &p_gc)
 
 	for (int i = 0; i < count; ++i) {
 		m_distances.push_back(std::vector<float>());
+		m_ldistances.push_back(std::vector<float>());
+		m_rdistances.push_back(std::vector<float>());
 
 		const Race::TrackSegment &seg = m_triangulator.getSegment(i);
 		const std::vector<Race::TrackSegment::PointPair> &pairs =
@@ -398,6 +569,8 @@ void LevelImpl::loadTrackTexture(CL_GraphicContext &p_gc)
 			}
 
 			m_distances[i].push_back((distL + distR) / 2);
+			m_ldistances[i].push_back(distL);
+			m_rdistances[i].push_back(distR);
 
 			prevPointL = pair.m_left;
 			prevPointR = pair.m_right;
@@ -415,9 +588,95 @@ void LevelImpl::loadTrackTexture(CL_GraphicContext &p_gc)
 
 }
 
+void LevelImpl::loadSandTexture(CL_GraphicContext &p_gc)
+{
+	m_sandTexture = CL_Texture(
+			"race/street_sand", Gfx::Stage::getResourceManager(), p_gc
+	);
+
+	m_sandTexture.set_wrap_mode(cl_wrap_clamp_to_edge, cl_wrap_repeat);
+	m_sandTexture.set_generate_mipmap(true);
+	m_sandTexture.set_mag_filter(cl_filter_linear);
+	m_sandTexture.set_min_filter(cl_filter_linear);
+}
+
+void LevelImpl::loadCracks(CL_GraphicContext &p_gc)
+{
+	for (int i = 0; i < CRACKS_COUNT; ++i) {
+		m_crackSprites[i] =
+				CL_Sprite(
+						p_gc,
+						cl_format("race/street_crack_%1", (i + 1)),
+						Stage::getResourceManager()
+				);
+	}
+
+
+	// generate cracks
+	static int MIN_CRACK_DIST = 50;
+	static int MAX_CRACK_DIST = 200;
+
+	const Race::Track &track = m_levelLogic.getTrack();
+	const int count = track.getPointCount();
+
+	CL_Pointf prevPointL, prevPointR;
+	bool hasFirst = false;
+	float dist = 0.0f;
+	float next = rand() % (MAX_CRACK_DIST - MIN_CRACK_DIST) + MIN_CRACK_DIST;
+
+	for (int i = 0; i < count; ++i) {
+		const Race::TrackSegment &seg = m_triangulator.getSegment(i);
+		const std::vector<Race::TrackSegment::PointPair> &pairs =
+				seg.getPointPairs();
+
+		foreach (const Race::TrackSegment::PointPair &pair, pairs) {
+			if (hasFirst) {
+				dist += (
+						Units::toWorld(prevPointL.distance(pair.m_left))
+						+ Units::toWorld(prevPointR.distance(pair.m_right))
+				) / 2;
+			} else {
+				hasFirst = true;
+			}
+
+			if (dist >= next) {
+				// if distance reached, then put the crack
+				const CL_Pointf pos((pair.m_left + pair.m_right) / 2);
+				const int index = rand() % CRACKS_COUNT;
+
+				m_crackPositions.push_back(Crack(pos, index));
+
+				// add new next distance
+				next +=
+						rand()
+						% (MAX_CRACK_DIST - MIN_CRACK_DIST) + MIN_CRACK_DIST;
+			}
+
+			prevPointL = pair.m_left;
+			prevPointR = pair.m_right;
+		}
+	}
+
+}
+
 Race::TrackTriangulator &Level::getTrackTriangulator()
 {
 	return const_cast<Race::TrackTriangulator&>(m_impl->m_triangulator);
+}
+
+void LevelImpl::expand(CL_Rectf *p_rect, const CL_Pointf &p_point)
+{
+	if (p_point.x < p_rect->left) {
+		p_rect->left = p_point.x;
+	} else if (p_point.x > p_rect->right) {
+		p_rect->right = p_point.x;
+	}
+
+	if (p_point.y < p_rect->top) {
+		p_rect->top = p_point.y;
+	} else if (p_point.y > p_rect->bottom) {
+		p_rect->bottom = p_point.y;
+	}
 }
 
 } // namespace
